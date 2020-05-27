bbecb7db308b16db4f89ed56d7dcd0cdcf556871
laurent b <wrmultitudes@gmail.com>
2020-05-27 16:45:31 +0200

    Update Numeric.swift

--- BriskScript/Sources/Brisk/Numeric.swift
+++ BriskScript/Sources/Brisk/Numeric.swift
@@ -41,6 +41,33 @@ func /<I: BinaryInteger, F: BinaryFloatingPoint>(lhs: F, rhs: I) -> F {
 }
 
 // ------------------------------------------
+// How to add two Tuples with the + operator in Swift
+// Good for adding two coordinates together
+// ex 
+// let position = (x: 0, y: 0)
+// let rightMove = (x: 0, y: 1)
+// let downMove = (x: 1, y: 0)
+// let positionA = position + rightMove
+// let positionB = position + downMove
+
+func +<T : Numeric>(_ a: (T, T), _ b: (T, T)) -> (T, T) {
+    return (a.0 + b.0, a.1 + b.1)
+}
+
+func -<T : Numeric>(_ a: (T, T), _ b: (T, T)) -> (T, T) {
+    return (a.0 - b.0, a.1 - b.1)
+}
+
+func *<T : Numeric>(_ a: (T, T), _ b: (T, T)) -> (T, T) {
+    return (a.0 * b.0, a.1 * b.1)
+}
+
+func /<T : Numeric>(_ a: (T, T), _ b: (T, T)) -> (T, T) {
+    return (a.0 / b.0, a.1 / b.1)
+}
+
+
+// ------------------------------------------
 // How to replicate the ** operator in Swift
 // 
 
